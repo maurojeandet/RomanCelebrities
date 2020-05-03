f57cabe93a5568518ccc9a55662b0647700d0da6
Mauro Jeandet <mauro.jeandet@southworks.com>
2020-05-03 16:12:50 -0300

    Updates

---
+++ precache-manifest.2d2b098177071f8dcc0191c0bfee0462.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed21c5d1c8b298913f9a6ee3304813e",
+    "url": "/RomanCelebrities/index.html"
+  },
+  {
+    "revision": "d65122517b901986cc12",
+    "url": "/RomanCelebrities/static/css/main.82cf8973.chunk.css"
+  },
+  {
+    "revision": "c7f6f659874c0167ab1c",
+    "url": "/RomanCelebrities/static/js/2.9ab4bbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RomanCelebrities/static/js/2.9ab4bbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65122517b901986cc12",
+    "url": "/RomanCelebrities/static/js/main.590b4fd5.chunk.js"
+  },
+  {
+    "revision": "b1f398804ca4ce44b8ed",
+    "url": "/RomanCelebrities/static/js/runtime-main.d97e910f.js"
+  },
+  {
+    "revision": "ff7f0df0eca491e54e453ff0aa575caa",
+    "url": "/RomanCelebrities/static/media/Roman SD.ff7f0df0.ttf"
+  },
+  {
+    "revision": "97f30d3b3b64afc7190bac1326c54998",
+    "url": "/RomanCelebrities/static/media/antoninus_pius.97f30d3b.png"
+  },
+  {
+    "revision": "5c43258f343ed567792e2d9e3df970ed",
+    "url": "/RomanCelebrities/static/media/carcalla.5c43258f.png"
+  },
+  {
+    "revision": "e605b6cacf2cb0d8cd7e5d368db9853e",
+    "url": "/RomanCelebrities/static/media/julius_caesar.e605b6ca.png"
+  },
+  {
+    "revision": "fa81d08d7e724bed138cb50c75ab10b4",
+    "url": "/RomanCelebrities/static/media/marcus_aurelius.fa81d08d.png"
+  },
+  {
+    "revision": "0287ab6a89804e36207797f4e36b7dea",
+    "url": "/RomanCelebrities/static/media/marcus_cicero.0287ab6a.png"
+  },
+  {
+    "revision": "6c92a2ed4dc680ffb5e26704c99d9ec8",
+    "url": "/RomanCelebrities/static/media/marcus_traianus.6c92a2ed.png"
+  },
+  {
+    "revision": "545d4c2a201cf1d7dea14d13adafb051",
+    "url": "/RomanCelebrities/static/media/octavius_augustus.545d4c2a.png"
+  },
+  {
+    "revision": "23918ba2366029dfc970de3630432155",
+    "url": "/RomanCelebrities/static/media/papyrus.23918ba2.png"
+  }
+]);
